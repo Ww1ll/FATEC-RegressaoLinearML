ed3751cc0dcdb2c182472c166b81d03cc22a2030
William <109118303+Ww1ll@users.noreply.github.com>
2024-09-18 19:32:16 -0300

    add projeto

---
+++ RegressaoLinear.js
@@ -0,0 +1,36 @@
+function media(array) {
+    const soma = array.reduce((acc, val) => acc + val, 0);
+    return soma / array.length;
+}
+
+function regressaoLinear(x, y) {
+    if (x.length !== y.length) {
+        throw new Error("Os arrays x e y devem ter o mesmo comprimento");
+    }
+
+    const n = x.length;
+    const xMedia = media(x);
+    const yMedia = media(y);
+
+    let numerador = 0;
+    let denominador = 0;
+
+    for (let i = 0; i < n; i++) {
+        numerador += (x[i] - xMedia) * (y[i] - yMedia);
+        denominador += (x[i] - xMedia) ** 2;
+    }
+
+    const m = numerador / denominador; 
+
+    return m;
+}
+
+for (let i = 1; i < 5; i++) {
+    const amostra = require(`./Data/json/Amostra${i}.json`);
+
+    const x = amostra.map(item => item.x); 
+    const y = amostra.map(item => item.y);
+
+    const coeficienteAngular = regressaoLinear(x, y);
+    console.log(`Coeficiente Angular Amostra ${i}: ${coeficienteAngular}`)
+}
